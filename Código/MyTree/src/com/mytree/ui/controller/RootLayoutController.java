ba23a6f72265b7d7c95a36bc17b11200fbc76e9e
María Inés Ricaldoni <mariaines@gmail.com>
2019-06-02 02:28:05 -0300

    titulos

--- Código/MyTree/src/com/mytree/ui/controller/RootLayoutController.java
+++ Código/MyTree/src/com/mytree/ui/controller/RootLayoutController.java
@@ -6,33 +6,42 @@
 package com.mytree.ui.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public final class RootLayoutController extends BaseController {
+    
+    @FXML
+    private Label title;
 
     public RootLayoutController() {
     }
 
     @Override
     protected void onInitialize() {
+        title.setText("Árbol Gráfico");
     }
 
     @FXML
     private void handleTreeButton() {
+        title.setText("Árbol Gráfico");
         getNavigationManager().showTree();
     }
     
     @FXML
     private void handleTextualTreeButton() {
+        title.setText("Árbol Textual");
         getNavigationManager().showTextualTree();
     }
 
     @FXML
     private void handleUsersButton() {
+        title.setText("Familia");
         getNavigationManager().showUsers();
     }
 
     @FXML
     private void handleAttachmentButton() {
+        title.setText("Adjuntos");
         getNavigationManager().showAttachments();
     }
 }
